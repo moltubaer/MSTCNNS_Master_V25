54b66888b243e26c6e82451b712e28191af4d9e5
Alexander Moltu <alexandermoltu@gmail.com>
2025-02-12 15:40:59 +0100

    fixed destination directory

--- Open5GS/migrate_config_open5gs.sh
+++ Open5GS/migrate_config_open5gs.sh
@@ -3,36 +3,41 @@
 SOURCE_REPO="~/MSTCNNS_MASTER_V25/Open5gs/config" 
 DESTINATION_DIR="~/docker_open5gs/config"
 
-CONFIG_FILES=(
-  "amf.yaml"
-  "smf.yaml"
-  "upf.yaml"
-  "nrf.yaml"
-  "ausf.yaml"
-  "udm.yaml"
-  "udr.yaml"
-  "pcf.yaml"
-  "bsf.yaml"
-  "nssf.yaml"
+# Mapping of configuration files to correct subdirectories
+declare -A CONFIG_MAP
+CONFIG_MAP=(
+  ["amf.yaml"]="amf/amf.yaml"
+  ["smf.yaml"]="smf/smf.yaml"
+  ["upf.yaml"]="upf/upf.yaml"
+  ["nrf.yaml"]="nrf/nrf.yaml"
+  ["ausf.yaml"]="ausf/ausf.yaml"
+  ["udm.yaml"]="udm/udm.yaml"
+  ["udr.yaml"]="udr/udr.yaml"
+  ["pcf.yaml"]="pcf/pcf.yaml"
+  ["bsf.yaml"]="bsf/bsf.yaml"
+  ["nssf.yaml"]="nssf/nssf.yaml"
 )
 
 # Stop running Open5GS containers before updating config
 echo "Stopping Open5GS Docker containers..."
-docker compose -f /path/to/docker_open5gs/sa-deploy.yaml down
+# docker compose -f "$DESTINATION_DIR/sa-deploy.yaml" down
 
-# Copy configuration files
+# Copy configuration files to their respective locations
 echo "Copying Open5GS configuration files..."
-for FILE in "${CONFIG_FILES[@]}"; do
-    if [ -f "$SOURCE_REPO/$FILE" ]; then
-        cp "$SOURCE_REPO/$FILE" "$DESTINATION_DIR/"
-        echo "Copied: $FILE"
+for FILE in "${!CONFIG_MAP[@]}"; do
+    SRC_FILE="$SOURCE_REPO/$FILE"
+    DEST_FILE="$DESTINATION_DIR/${CONFIG_MAP[$FILE]}"
+    
+    if [ -f "$SRC_FILE" ]; then
+        cp "$SRC_FILE" "$DEST_FILE"
+        echo "Copied: $FILE -> $DEST_FILE"
     else
         echo "Warning: $FILE not found in source repo!"
     fi
 done
 
 # Restart Open5GS containers
-# echo "Restarting Open5GS Docker containers..."
-# docker compose -f /path/to/docker_open5gs/sa-deploy.yaml up -d
+echo "Restarting Open5GS Docker containers..."
+# docker compose -f "$DESTINATION_DIR/sa-deploy.yaml" up -d
 
 echo "Migration complete!"
