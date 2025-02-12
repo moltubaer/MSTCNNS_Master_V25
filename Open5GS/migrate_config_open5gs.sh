7a0b46db64e08a144069167a018f09c3429341ef
Alexander Moltu <alexandermoltu@gmail.com>
2025-02-12 15:46:30 +0100

    fixed wrong paths

--- Open5GS/migrate_config_open5gs.sh
+++ Open5GS/migrate_config_open5gs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SOURCE_REPO="./config" 
-DESTINATION_DIR="../docker_open5gs"
+SOURCE_REPO="/home/ubuntu/MSTCNNS_Master_V25/Open5gs/config" 
+DESTINATION_DIR="/home/ubuntu/docker_open5gs"
 
 # Mapping of configuration files to correct subdirectories
 declare -A CONFIG_MAP
