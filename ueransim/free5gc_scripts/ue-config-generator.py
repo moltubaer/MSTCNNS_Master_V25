b34f6ea18cd6817c6b3deb72cd5f954c1dc95693
Alexander Moltu <alexandermoltu@gmail.com>
2025-04-07 12:28:13 +0200

    free5gc ue generater

--- ueransim/free5gc_scripts/ue-config-generator.py
+++ ueransim/free5gc_scripts/ue-config-generator.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-def generate_open5gs_ue_configs(template_path, output_dir, start_index, end_index, base_imsi_str):
+def generate_free5gc_ue_configs(template_path, output_dir, start_index, end_index, base_imsi_str):
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     with open(template_path, 'r') as file:
@@ -13,7 +13,7 @@ def generate_open5gs_ue_configs(template_path, output_dir, start_index, end_inde
         imsi = f'imsi-{base_number + (i - start_index):015d}'
         content = template.replace("supi: 'imsi-001010000000001'", f"supi: '{imsi}'")
 
-        filename = f"open5gs-ue-{i}.yaml"
+        filename = f"free5gc-ue-{i}.yaml"
         filepath = os.path.join(output_dir, filename)
         with open(filepath, 'w') as out_file:
             out_file.write(content)
@@ -21,10 +21,10 @@ def generate_open5gs_ue_configs(template_path, output_dir, start_index, end_inde
         print(f"Generated {filename} with IMSI {imsi}")
 
 # Example usage
-generate_open5gs_ue_configs(
-    template_path='../config/open5gs-ue.yaml',
+generate_free5gc_ue_configs(
+    template_path='../config/free5gc-ue.yaml',
     output_dir='/home/ubuntu/UERANSIM/config/tests',
     start_index=1,
     end_index=10,
-    base_imsi_str="001010000000010"
+    base_imsi_str="001010000000001"
 )
