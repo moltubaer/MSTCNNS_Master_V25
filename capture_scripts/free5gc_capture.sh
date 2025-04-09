d2025aa772256c701690faa2bdaafa18e8ea7b3d
Alexander Moltu <alexandermoltu@gmail.com>
2025-04-09 11:33:09 +0200

    some small changes

--- capture_scripts/free5gc_capture.sh
+++ capture_scripts/free5gc_capture.sh
@@ -5,7 +5,7 @@
 # ========================
 
 # List of Free5gc containers to capture from
-containers=("free5gc_amf" "free5gc_smf" "free5gc_upf")
+containers=("free5gc_amf" "free5gc_smf" "free5gc_upf" "free5gc_udm" "free5gc_ausf" "free5gc_pcf" "free5gc_nssf" "free5gc_udr" "free5gc_nrf")
 
 # Interface inside the containers (use "any" to capture all)
 container_interface="any"
