eaccd09462cffb9c48b03c8dc280a96dc7e54861
Alexander Moltu <alexandermoltu@gmail.com>
2025-04-25 14:23:04 +0200

    small fix

--- data_analysis/ngap_id.py
+++ data_analysis/ngap_id.py
@@ -4,10 +4,17 @@ import csv
 import os
 from collections import defaultdict
 
-# === CLI ARGS ===
+
+num = 100
+# Argument parser for optional input file
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", required=True, help="Input JSON file")
-parser.add_argument("--plot", action="store_true", help="Enable latency visualization")
+parser.add_argument(
+    "-f", "--file",
+    type=str,
+    # default="./captures/pdu-est-re.json",
+    default="/home/ubuntu/pcap_captures/" + str(num) + ".json",
+    help="Path to the input JSON file"
+)
 args = parser.parse_args()
 
 with open(args.file) as f:
@@ -108,14 +115,15 @@ for event_type, rows in grouped_csv_rows.items():
     summary_filename = f"{event_type.lower().replace(' ', '_')}_summary.csv"
     write_header = not os.path.exists(summary_filename) or os.stat(summary_filename).st_size == 0
     with open(summary_filename, "a", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=["event_type", "total_duration", "average_latency", "num_ues", "sum_latencies"])
+        writer = csv.DictWriter(f, fieldnames=["event_type", "total_duration", "average_latency", "num_ues", "effective_num_ues", "sum_latencies"])
         if write_header:
             writer.writeheader()
         writer.writerow({
             "event_type": event_type,
             "total_duration": f"{total_duration:.6f}",
             "average_latency": f"{average_latency:.6f}",
-            "num_ues": effective_num_ues,
+            "num_ues": num,
+            "effective_num_ues": effective_num_ues,
             "sum_latencies": f"{sum_all_latencies:.6f}"
         })
     print(f"📄 Summary written to {summary_filename}")
