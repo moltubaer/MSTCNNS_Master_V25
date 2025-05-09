2a07181421c3eda2156ec805dba65e52abd26b14
= <=>
2025-05-09 13:54:34 +0200

    updated open5gs capture scripts

--- capture_scripts/remote_open5gs.sh
+++ capture_scripts/remote_open5gs.sh
@@ -11,37 +11,22 @@ ueransim="ubuntu@10.100.51.82"
 system_script="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/capture_with_metrics.py"
 core1_script="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/open5gs_capture.sh"
 ueransim_script="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/ueransim_capture.sh"
-# ueransim_script="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/control_plane_tests/single_ue_reg.sh"
 
-# Default duration
-DURATION=120
+# Default value if not specified
+DURATION=${1:-5}
 
-# Parse optional arguments
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    --duration)
-      DURATION="$2"
-      shift 2
-      ;;
-    *)
-      echo "❌ Unknown argument: $1"
-      exit 1
-      ;;
-  esac
-done
+echo "Duration is $DURATION"
 
 # ========================
 # EXECUTE REMOTE SCRIPTS
 # ========================
 
 echo "[*] Running script on $core1..."
-# ssh -tt "$core1" "sudo bash $core1_script $DURATION && python3 $system_script -d $DURATION" &
 ssh -tt "$core1" "sudo bash $core1_script $DURATION" &
 ssh -tt "$core1" "sudo python3 $system_script -d $DURATION" &
 PID1=$!
 
 echo "[*] Running script on $ueransim..."
-# ssh -tt "$ueransim" "sudo bash $ueransim_script $DURATION && python3 $system_script -d $DURATION" &
 ssh -tt "$ueransim" "sudo bash $ueransim_script $DURATION" &
 ssh -tt "$ueransim" "sudo python3 $system_script -d $DURATION" &
 PID2=$!
