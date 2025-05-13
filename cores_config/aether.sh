ab2bfd7dc1cf35d9ef85a369b474074479bb7191
Marcus Johannessen <69755168+MarcusJoha@users.noreply.github.com>
2025-05-13 17:57:14 +0200

    Delete cores_config directory
    should be defined in .gitignore

--- cores_config/aether.sh
+++
@@ -1,8 +0,0 @@
-CORE="aether"
-CORE_CONNECTION="ubuntu@10.100.51.81"
-UERANSIM_CONNECTION="ubuntu@10.100.51.82"
-CORE_KEY="~/.ssh/core2.key"
-UERANSIM_KEY="~/.ssh/marc_ueransim.key"
-CORE_CAPTURE_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/aether_capture.sh"
-UERANSIM_CAPTURE_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/ueransim_capture.sh"
-
--- cores_config/free5gc.sh
+++
@@ -1,8 +0,0 @@
-CORE="free5gc"
-CORE_CONNECTION="ubuntu@10.100.51.80"
-UERANSIM_CONNECTION="ubuntu@10.100.51.82"
-CORE_KEY="~/.ssh/core1.key"
-UERANSIM_KEY="~/.ssh/marc_ueransim.key"
-CORE_CAPTURE_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/free5gc_capture.sh"
-UERANSIM_CAPTURE_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/ueransim_capture.sh"
-
