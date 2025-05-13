de2cd05b656da4337a8b5acc8a661ab8b076cdba
Marcus Johannessen <marcus-j@online.no>
2025-05-13 17:56:13 +0200

    fix for cores config

--- cores_config/aether.sh
+++ cores_config/aether.sh
@@ -5,7 +5,4 @@ CORE_KEY="~/.ssh/core2.key"
 UERANSIM_KEY="~/.ssh/marc_ueransim.key"
 CORE_CAPTURE_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/aether_capture.sh"
 UERANSIM_CAPTURE_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/ueransim_capture.sh"
-# UERANSIM_RUN_UES_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/test_scripts/run_ues.py"
-# TEST_SCRIPT="run_ues.py" # [run_ues.py | pdu_sessions.py | ue_dereg.py]
 
-DUARTION="120"
--- cores_config/open5gs.sh
+++ cores_config/open5gs.sh
@@ -1,11 +1,8 @@
-CORE="free5gc"
+CORE="open5gs"
 CORE_CONNECTION="ubuntu@10.100.51.80"
 UERANSIM_CONNECTION="ubuntu@10.100.51.82"
 CORE_KEY="~/.ssh/core1.key"
 UERANSIM_KEY="~/.ssh/marc_ueransim.key"
 CORE_CAPTURE_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/free5gc_capture.sh"
 UERANSIM_CAPTURE_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/capture_scripts/ueransim_capture.sh"
-# UERANSIM_RUN_UES_SCRIPT="/home/ubuntu/MSTCNNS_Master_V25/test_scripts/run_ues.py"
-TEST_SCRIPT="run_ues.py" # [run_ues.py | pdu_sessions.py | ue_dereg.py]
 
-DUARTION="120"
